bfa3ae68b06608a6f9a4f66b95c5b4920d6227c0
Villermen <villermen@gmail.com>
2017-02-28 17:39:48 +0100

    Pass through files in PHP built-in web server

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
+require_once(__DIR__ . "/../vendor/autoload.php");
+
 use Villermen\WebFileBrowser\App;
 
-require_once(__DIR__ . "/../vendor/autoload.php");
+// Pass through existing files for PHP built-in web server
+if (php_sapi_name() == "cli-server" && strlen($_SERVER["REQUEST_URI"]) > 1 && file_exists($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
+    return false;
+}
 
 $app = new App(__DIR__ . "/../config/config.yml");
 $app->run();
