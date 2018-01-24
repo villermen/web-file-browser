fb35bae42d1a19c1f92086a16770e483c24dd772
Villermen <villermen@gmail.com>
2018-01-24 14:44:21 +0100

    Styled 404 page

--- src/Villermen/WebFileBrowser/App.php
+++ src/Villermen/WebFileBrowser/App.php
@@ -2,11 +2,13 @@
 
 namespace Villermen\WebFileBrowser;
 
+use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig_Environment;
 use Twig_Loader_Filesystem;
 use Villermen\DataHandling\DataHandling;
+use Villermen\DataHandling\DataHandlingException;
 
 class App
 {
@@ -45,46 +47,69 @@ class App
 
     public function run()
     {
-        $request = Request::createFromGlobals();
+        $response = $this->handleRequest(Request::createFromGlobals());
+        $response->send();
+    }
 
+    /**
+     * Turns the supplied request into a rendered response.
+     *
+     * @param Request $request
+     * @return Response
+     * @throws Exception
+     */
+    private function handleRequest(Request $request): Response
+    {
         $configuration = new Configuration($this->configFile, $request);
 
         $requestedDirectory = DataHandling::formatDirectory($configuration->getRoot(), $request->getPathInfo());
 
         $accessible = $configuration->isDirectoryAccessible($requestedDirectory, $reason);
 
-        if ($accessible) {
-            $directory = $configuration->getDirectory($requestedDirectory);
+        $twig = $this->getTwig($configuration);
 
-            $path = "/" . rtrim($configuration->getRelativePath($directory->getPath()), "/");
-
-            return (new Response($this->getTwig()->render("listing.html.twig", [
-                "configuration" => $configuration,
-                "directory" => $directory,
-                "pathParts" => $this->getPathParts($path, $configuration, $directory),
-                "path" => $path
-            ])))->send();
+        // Show a page not found page when the directory does not exist or is not accessible
+        if (!$accessible) {
+            return new Response($twig->render("not-found.html.twig"), Response::HTTP_NOT_FOUND);
         }
 
-        // TODO: This could be made to look fancier...
-        return (new Response("404 Not Found", 404))->send();
+        $directory = $configuration->getDirectory($requestedDirectory);
+
+        $path = "/" . rtrim($configuration->getRelativePath($directory->getPath()), "/");
+
+        return new Response($twig->render("listing.html.twig", [
+            "directory" => $directory,
+            "pathParts" => $this->getPathParts($path, $configuration, $directory),
+            "path" => $path
+        ]));
     }
 
-    private function getTwig(): Twig_Environment
+    private function getTwig(Configuration $configuration): Twig_Environment
     {
         if ($this->twig) {
             return $this->twig;
         }
 
         $twigLoader = new Twig_Loader_Filesystem("views/");
-        $this->twig = new Twig_Environment($twigLoader, [
+        $twig = new Twig_Environment($twigLoader, [
             "autoescape" => "html",
             "strict_variables" => true
         ]);
 
-        return $this->twig;
+        $twig->addGlobal("configuration", $configuration);
+
+        $this->twig = $twig;
+
+        return $twig;
     }
 
+    /**
+     * @param string $path
+     * @param Configuration $configuration
+     * @param Directory $directory
+     * @return array
+     * @throws DataHandlingException
+     */
     private function getPathParts(string $path, Configuration $configuration, Directory $directory): array
     {
         // Construct path parts for navigation
