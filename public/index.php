1752704ce5ae0a7449bb657992a9792fea0c1f36
Villermen <villermen@gmail.com>
2025-11-16 14:32:00 +0100

    Update dependencies, add dev-server Composer script

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+// Let dev-server handle regular files.
+if (php_sapi_name() === 'cli-server') {
+    if (preg_match('~^(/[^#?]+)~', $_SERVER['REQUEST_URI'], $matches)) {
+        if (is_file(__DIR__ . $matches[1])) {
+            return false;
+        }
+    }
+}
+
 require_once(__DIR__ . "/../vendor/autoload.php");
 
 $app = new Villermen\WebFileBrowser\App(__DIR__ . "/../config/config.yml");
